2b2cd661ff0c42186705a77e2dd9333f7091a54e
sunghyeok.choi <shyeokchoi@gmail.com>
2026-08-02 22:57:55 +0900

    Implement List.length with foldRight

--- src/main/scala/fpinscala/exercises/datastructures/List.scala
+++ src/main/scala/fpinscala/exercises/datastructures/List.scala
@@ -106,7 +106,7 @@ object List: // `List` companion object. Contains functions for creating and wor
 
     reverse(reversed, Nil)
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int = foldRight(l, 0, (_, b) => 1 + b)
 
   def foldLeft[A, B](l: List[A], acc: B, f: (B, A) => B): B = ???
 
