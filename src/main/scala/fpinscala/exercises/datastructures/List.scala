3ff50e30146ee778f7508047ab8d6f0cde73becb
sunghyeok.choi <shyeokchoi@gmail.com>
2026-08-24 01:02:55 +0900

    Implement flatMap with foldRight

--- src/main/scala/fpinscala/exercises/datastructures/List.scala
+++ src/main/scala/fpinscala/exercises/datastructures/List.scala
@@ -148,7 +148,8 @@ object List: // `List` companion object. Contains functions for creating and wor
   def filter[A](as: List[A], f: A => Boolean): List[A] =
     foldRight(as, Nil: List[A], (x, acc) => if (f(x)) Cons(x, acc) else acc)
 
-  def flatMap[A, B](as: List[A], f: A => List[B]): List[B] = ???
+  def flatMap[A, B](as: List[A], f: A => List[B]): List[B] =
+    foldRight(as, Nil: List[B], (x, acc) => append(f(x), acc))
 
   def filterViaFlatMap[A](as: List[A], f: A => Boolean): List[A] = ???
 
