deab3c18338fc659c6727e3cb52888e7155d4dd3
sunghyeok.choi <shyeokchoi@gmail.com>
2026-08-24 00:55:15 +0900

    Implement doubleToString with foldRight

--- src/main/scala/fpinscala/exercises/datastructures/List.scala
+++ src/main/scala/fpinscala/exercises/datastructures/List.scala
@@ -139,7 +139,8 @@ object List: // `List` companion object. Contains functions for creating and wor
   def incrementEach(l: List[Int]): List[Int] =
     foldRight(l, Nil: List[Int], (x, acc) => Cons(x + 1, acc))
 
-  def doubleToString(l: List[Double]): List[String] = ???
+  def doubleToString(l: List[Double]): List[String] =
+    foldRight(l, Nil: List[String], (x, acc) => Cons(x.toString, acc))
 
   def map[A, B](l: List[A], f: A => B): List[B] = ???
 
