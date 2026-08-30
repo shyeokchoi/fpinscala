4e02558e8c1cc49e4ed1908ae51c3351558230a5
sunghyeok.choi <shyeokchoi@gmail.com>
2026-08-31 00:30:09 +0900

    format

--- src/main/scala/fpinscala/exercises/datastructures/Tree.scala
+++ src/main/scala/fpinscala/exercises/datastructures/Tree.scala
@@ -5,26 +5,26 @@ enum Tree[+A]:
   case Branch(left: Tree[A], right: Tree[A])
 
   def size: Int = this match
-    case Leaf(_) => 1
+    case Leaf(_)      => 1
     case Branch(l, r) => 1 + l.size + r.size
 
   def depth: Int = ???
 
   def map[B](f: A => B): Tree[B] = ???
 
-  def fold[B](f: A => B, g: (B,B) => B): B = ???
-  
+  def fold[B](f: A => B, g: (B, B) => B): B = ???
+
   def sizeViaFold: Int = ???
-  
+
   def depthViaFold: Int = ???
-  
+
   def mapViaFold[B](f: A => B): Tree[B] = ???
 
 object Tree:
 
   def size[A](t: Tree[A]): Int = t match
-    case Leaf(_) => 1
-    case Branch(l,r) => 1 + size(l) + size(r)
+    case Leaf(_)      => 1
+    case Branch(l, r) => 1 + size(l) + size(r)
 
   extension (t: Tree[Int]) def firstPositive: Int = ???
 
