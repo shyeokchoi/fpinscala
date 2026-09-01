c264067dc1ad06772a2fd5f6ba3d40191f4557fc
sunghyeok.choi <shyeokchoi@gmail.com>
2026-09-02 00:41:30 +0900

    Update Option exercise scaffolding

--- src/main/scala/fpinscala/exercises/errorhandling/Option.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Option.scala
@@ -9,27 +9,37 @@ enum Option[+A]:
 
   def map[B](f: A => B): Option[B] = ???
 
-  def getOrElse[B>:A](default: => B): B = ???
+  def getOrElse[B >: A](default: => B): B = ???
 
+  // limit: don't use pattern matching
   def flatMap[B](f: A => Option[B]): Option[B] = ???
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
+  // limit: don't use pattern matching
+  def orElse[B >: A](ob: => Option[B]): Option[B] = ???
 
+  // limit: don't use pattern matching
   def filter(f: A => Boolean): Option[A] = ???
 
 object Option:
 
   def failingFn(i: Int): Int =
-    val y: Int = throw new Exception("fail!") // `val y: Int = ...` declares `y` as having type `Int`, and sets it equal to the right hand side of the `=`.
+    val y: Int =
+      throw new Exception(
+        "fail!"
+      ) // `val y: Int = ...` declares `y` as having type `Int`, and sets it equal to the right hand side of the `=`.
     try
       val x = 42 + 5
       x + y
-    catch case e: Exception => 43 // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
+    catch
+      case e: Exception =>
+        43 // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
 
   def failingFn2(i: Int): Int =
     try
       val x = 42 + 5
-      x + ((throw new Exception("fail!")): Int) // A thrown Exception can be given any type; here we're annotating it with the type `Int`
+      x + ((throw new Exception(
+        "fail!"
+      )): Int) // A thrown Exception can be given any type; here we're annotating it with the type `Int`
     catch case e: Exception => 43
 
   def mean(xs: Seq[Double]): Option[Double] =
@@ -38,7 +48,7 @@ object Option:
 
   def variance(xs: Seq[Double]): Option[Double] = ???
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
 
   def sequence[A](as: List[Option[A]]): Option[List[A]] = ???
 
